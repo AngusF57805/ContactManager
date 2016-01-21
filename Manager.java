7f3099af69ee908fd4599db99cfaa23ebb022ea9
Angus <angus.findlay.mb@gmail.com>
2016-01-21 19:01:49 +0000

    changed complicated page numbers to fancy enums

--- Manager.java
+++ Manager.java
@@ -10,11 +10,17 @@ class Manager {
 	static Set<Meeting> meetings;
 	static String tempName;
 
+	static enum Page {
+		HOME,
+		LISTC, ADDC_1, ADDC_2, EDITC_1, EDITC_2, FINDC,
+		LISTM, ADDM_1, ADDM_2, EDITM_1, EDITM_2, FINDM
+	}
+
 	public static void main (String[] args) {
 		initializeSets (); //initialize the contacts/ meetings HashSets
 		createContacts (); //from text file
 		createMeetings (); //from text file
-		setUi (0); //home page
+		setUi (Page.HOME); //home page
 	}
 
 	static void print (String str) {
@@ -64,70 +70,119 @@ class Manager {
 		return contacts.toArray(new Contact[contacts.size()])[n];
 	}
 
-	static void setUi (int page) {
+	static void setUi (Page page) {
 
 		switch (page) {
-			case 0:
+			case Page.HOME://home
 				print ("Welcome to the Contacts Manager, what would you like to do?\n");
 				//get input
 				checkInput (page);
 				break;
-			case 1:
+			case Page.LISTC://list c
 				print ("LIST OF CONTACTS:\n");
 				for (int i = 0; i < contacts.size(); i ++) {
 					print ("- " + findContact(i).id +  " : " + findContact(i).name + " : " + findContact(i).notes + "\n");
 				}
-				setUi (0);
+				setUi (Page.HOME);
 				break;
-			case 2:
+			case Page.ADDC_1://add c - 1
 				print ("type the FULL NAME of the contact:");
 				checkInput (page);
 				setUi (3);
 				break;
-			case 3:
+			case Page.ADDC_2://add c - 2
 				print ("type any NOTES about this person:");
 				checkInput (page);
 				print ("Contact '" + tempName + "' added!\n");
-				setUi (0);
+				setUi (Page.HOME);
+				break;
+			case Page.EDITC_1://edit c - 1
+				print ("type the new FULL NAME of the contact (leave blank to not change):");
+				checkInput (page);
+				setUi (5);
+				break;
+			case Page.EDITC_2://edit c - 2
+				print ("type any new NOTES about the contact (leave blank to not change):");
+				checkInput (page);
+				print ("Contact '" + tempName + "' edited.\n");
+				setUi (Page.HOME);
+				break;
+			case Page.FINDC://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.LISTM://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.ADDM_1://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.ADDM_2://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.EDITM_1://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.EDITM_2://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
+				break;
+			case Page.FINDM://
+				print ("");
+				checkInput (page);
+				setUi (Page.HOME);
 				break;
 			default:
 				print ("tried to go to a ui page that does not exist\n");
-				setUi (0);
+				setUi (Page.HOME);
 				break;
 		}
 	}
 
-	static void checkInput (int page) {
+	static void checkInput (Page page) {
 		switch (page) {
 			case 0:
 				switch (readScreen ()) {
-					case "list": //list
-						setUi (1);
+					case "list contacts": //list c
+						setUi (Page.LISTC);//list c
 						break;
-					case "add contact"://add contact
-						setUi (2);
+					case "add contact"://add c
+						setUi (Page.ADDC_1);//add c - 1
 						break;
-					case "edit contact"://edit contact
-						setUi (3);
+					case "edit contact"://edit c
+						setUi (Page.EDITC_1); //edit c - 1
 						break;
-					case "find contact"://find contact
-						setUi (4);
+					case "find contact"://find c
+						setUi (Page.FINDC);
 						break;
-					case "add meeting"://add meeting
-						setUi (5);
+					case "list meetings"://list m
+						setUi (Page.LISTM);
 						break;
-					case "edit meeting"://edit meeting
-						setUi (6);
+					case "add meeting"://add m
+						setUi (Page.ADDM_1;
 						break;
-					case "find meeting"://find meeting
-						setUi (7);
+					case "edit meeting"://edit m
+						setUi (Page.EDITM_1);
+						break;
+					case "find meeting"://find m
+						setUi (Page.FINDM);
 						break;
 					case "quit"://quit
 						flush ();
 						return;
 					default:
 						print ("unknown command - type help for a list of commands\n");
-						setUi (0);
+						setUi (Page.HOME);
 						break;
 				}
 
